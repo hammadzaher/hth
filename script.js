6fc097832b95c6fc76e8452e32540cbb90bdad6c
hammadzaher <zaheerhammad62@gmail.com>
2024-08-26 10:45:46 +0500

    my website

---
+++ script.js
@@ -0,0 +1,5 @@
+function hammad(){
+    let userInput1 = document.querySelector("#input1").value
+    let userInput2 = document.querySelector("#input2").value
+    document.querySelector("#result").innerText = Number(userInput1) * Number(userInput2)
+}
