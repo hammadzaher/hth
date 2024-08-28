f261cead613ac2903edde32c4a510bb7d6acffb0
hammadzaher <zaheerhammad62@gmail.com>
2024-08-28 09:40:00 +0500

    Add files via upload
    hammad

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function hammad(){
     let userInput1 = document.querySelector("#input1").value
     let userInput2 = document.querySelector("#input2").value
+    document.querySelector("#result").innerText = Number(userInput1) + Number(userInput2)
+}
+function hammad1(){
+    let userInput1 = document.querySelector("#input1").value
+    let userInput2 = document.querySelector("#input2").value
+    document.querySelector("#result").innerText = Number(userInput1) - Number(userInput2)
+}
+function hammad2(){
+    let userInput1 = document.querySelector("#input1").value
+    let userInput2 = document.querySelector("#input2").value
     document.querySelector("#result").innerText = Number(userInput1) * Number(userInput2)
+}
+function hammad3(){
+    let userInput1 = document.querySelector("#input1").value
+    let userInput2 = document.querySelector("#input2").value
+    document.querySelector("#result").innerText = Number(userInput1) / Number(userInput2)
 }
